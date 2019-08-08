1dcf8efa0efaa678fb51ee75d2a722630ac18f57
kaushikaramesh <47316306+kaushikaramesh@users.noreply.github.com>
2019-08-08 14:50:26 +0530

    Update creating.py

--- creating.py
+++ creating.py
@@ -1,13 +1,13 @@
-def check_Power(N,k):
-    if N <= 0 or k <=0:
+def check_Power(M,k):
+    if M <= 0 or k <=0:
         print "not a valid input"
     else:
         for i in range (1,20):
             x = k**i
-            if x == N :
+            if x == M :
                 print " True "
-                print k, "power ", i , "is", N
+                print k, "power ", i , "is", M
                 break
-            elif x> N:
+            elif x> M:
                 print "false"
                 break
